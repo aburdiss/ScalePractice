0c6802bfc276902beead95686bc39cc05d09dc15
aburdiss <aburdiss@gmail.com>
2020-10-10 14:59:05 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Hello, World!</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
