dd012f238d2973a3b7e0a1d2750b05e118237f10
aburdiss <aburdiss@gmail.com>
2020-11-01 15:08:55 -0500

    Added the basic functionality for the "More" screen.

--- Screens/More.js
+++ Screens/More.js
@@ -1,5 +1,72 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SectionList, Linking, StyleSheet, Pressable } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+const RESOURCES = [
+  {
+    id: '0',
+    type: 'link',
+    value: 'More Apps by Alexander Burdiss',
+    link: 'https://play.google.com/store/apps/developer?id=Alexander+Burdiss',
+  },
+  {
+    id: '1',
+    type: 'link',
+    value: 'Visit Ars Nova Publishing',
+    link: 'https://www.arsnovapublishing.com/',
+  },
+  {
+    id: '2',
+    type: 'link',
+    value: 'Visit Band Room Online',
+    link: 'https://www.bandroomonline.com/',
+  },
+];
+
+const ABOUT = [
+  {
+    id: '3',
+    type: 'text',
+    value: '© 2020 Alexander Burdiss',
+    link: null,
+  },
+  {
+    id: '4',
+    type: 'link',
+    value: 'Send Feedback',
+    link: 'mailto:aburdiss@gmail.com',
+  },
+];
+
+const ItemSeparator = () => {
+  return (
+    <View style={styles.listItemSeparator} />
+  );
+}
+
+const TextListItem = ({ item }) => {
+  return (
+    <View style={styles.listRowContainer}>
+      <Text>{item.value}</Text>
+    </View>
+  );
+}
+
+const LinkListItem = ({ item }) => {
+  return (
+    <Pressable
+      onPress={() => {
+        Linking.openURL(item.link);
+      }}
+    >
+      <View style={styles.listRowContainer}>
+        <Text style={styles.linkText}>{item.value}</Text>
+        <Ionicons name={'chevron-forward-outline'} size={30} color={'purple'} />
+      </View>
+    </Pressable>
+  )
+}
 
 
 /**
@@ -11,9 +78,51 @@ import { View, Text } from 'react-native';
 const More = () => {
   return (
     <View>
-      <Text>More</Text>
+      <SectionList
+        sections={[
+          {title: "Resources", data: RESOURCES},
+          {title: "About", data: ABOUT},
+        ]}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => (
+          item.type === 'link'
+          ? <LinkListItem item={item} />
+          : <TextListItem item={item} />
+        )}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={styles.listHeader}>{title}</Text>
+        )}
+      />
     </View>
   );
 };
 
 export default More;
+
+const styles = StyleSheet.create({
+  listItemSeparator: {
+    height: 0.5,
+    width: '100%',
+    backgroundColor: '#C8C8C8',
+  },
+  listHeader: {
+    textTransform: 'uppercase',
+    paddingLeft: 15,
+    paddingTop: 30,
+    paddingBottom: 5,
+  },
+  listRowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    paddingVertical: 8,
+    height: 45,
+    paddingHorizontal: 15,
+    borderTopWidth: 1,
+    borderTopColor: 'gray',
+  },
+  linkText: {
+    color: 'purple',
+  }
+});
