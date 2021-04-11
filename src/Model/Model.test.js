03ec31e79c502de6da32e340ac98979021147889
Alexander Burdiss <aburdiss@icloud.com>
2021-04-10 23:06:31 -0400

    Added functionality for testing https links in model

--- src/Model/Model.test.js
+++ src/Model/Model.test.js
@@ -1,7 +1,10 @@
 import 'react-native';
 import React from 'react';
 
+const fetch = require('node-fetch');
+
 import {scaleResourceData, arpeggioResourceData, getImagePath} from './Model';
+import {RESOURCES, ABOUT} from './MoreModel';
 
 test.each(scaleResourceData)('all scale images exist', (item) => {
   expect(getImagePath(item.id)).not.toBe(null);
@@ -9,4 +12,24 @@ test.each(scaleResourceData)('all scale images exist', (item) => {
 
 test.each(arpeggioResourceData)('all arpeggio images exist', (item) => {
   expect(getImagePath(item.id)).not.toBe(null);
+});
+
+describe('links', () => {
+  RESOURCES.map(async (resource) => {
+    test('resources links', async () => {
+      if (resource.type == 'link') {
+        const resp = await fetch(resource.link);
+        expect(resp.status).toEqual(200);
+      }
+    });
+  });
+
+  ABOUT.map(async (about) => {
+    test('about links', async () => {
+      if (about.type == 'link' && !about.link.startsWith('mailto')) {
+        const resp = await fetch(about.link);
+        expect(resp.status).toEqual(200);
+      }
+    });
+  });
 });
