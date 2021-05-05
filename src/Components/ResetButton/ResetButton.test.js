cd645a1cd9dad19261847c29d3703168e25f88fe
Alexander Burdiss <aburdiss@icloud.com>
2021-05-05 08:32:14 -0400

    Added Tests to ResetButton

--- src/Components/ResetButton/ResetButton.test.js
+++ src/Components/ResetButton/ResetButton.test.js
@@ -1,14 +1,20 @@
 import 'react-native';
 import React from 'react';
 import ResetButton from './ResetButton';
-import MockContext from '../../../jest/MockContext';
 
-import {render} from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 
 test('ResetButton renders correctly', () => {
-  render(
-    <MockContext>
-      <ResetButton />
-    </MockContext>,
-  );
+  const {queryByText} = render(<ResetButton handler={jest.fn()} />);
+  expect(queryByText(/Reset/)).toBeTruthy();
+});
+
+test('ResetButton Function calls correctly', () => {
+  const buttonHandler = jest.fn();
+  const {getByText} = render(<ResetButton handler={buttonHandler} />);
+  expect(buttonHandler).not.toHaveBeenCalled();
+  fireEvent.press(getByText(/Reset/));
+  expect(buttonHandler).toHaveBeenCalledTimes(1);
+  fireEvent.press(getByText(/Reset/));
+  expect(buttonHandler).toHaveBeenCalledTimes(2);
 });
