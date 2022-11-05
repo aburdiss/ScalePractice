837d0a836bbdac2dd3fdb1ce36da3737877c2e07
Alexander Burdiss <aburdiss@icloud.com>
2022-11-05 17:12:14 -0400

    Remove app.json, and unused babel config
    app.json is not required, and is only included because of historical reasons.
    see: react-native-community/cli#1113
    
    This commit also removes unused babel config from package.json, and a small
    reformat in project.pbxproj from Xcode

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+const appName = 'ScalePractice';
 
 AppRegistry.registerComponent(appName, () => App);
