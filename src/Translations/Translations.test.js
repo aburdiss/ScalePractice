4292c7cdd80e04f6cab2233b3066cb684c6505a5
Alexander Burdiss <aburdiss@icloud.com>
2023-01-08 08:13:47 -0500

    Ignore Warnings from all CocoaPods
    Add tests to all Spanish translations

--- src/Translations/Translations.test.js
+++ src/Translations/Translations.test.js
@@ -1,6 +1,6 @@
 import englishTranslations from './en.json';
 import frenchTranslations from './fr.json';
-// import spanishTranslations from '../../src/Translations/es.json';
+import spanishTranslations from './es.json';
 import chineseTranslations from './zh.json';
 import japaneseTranslations from './ja.json';
 import koreanTranslations from './ko.json';
@@ -11,9 +11,9 @@ test.each(englishTranslationList)('french translations all exist', (item) => {
   expect(frenchTranslations).toHaveProperty(item);
 });
 
-// test.each(englishTranslationList)('spanish translations all exist', (item) => {
-//   expect(spanishTranslations).toHaveProperty(item);
-// });
+test.each(englishTranslationList)('spanish translations all exist', (item) => {
+  expect(spanishTranslations).toHaveProperty(item);
+});
 
 test.each(englishTranslationList)('chinese translations all exist', (item) => {
   expect(chineseTranslations).toHaveProperty(item);
